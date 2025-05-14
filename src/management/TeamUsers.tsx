1fb30fe52d4ca03d157c91e9657b27b9aada2403
Yogesh <yogesh@celestialsys.com>
2025-05-14 13:00:31 +0530

    cleanup team page - fix #17

--- src/management/TeamUsers.tsx
+++ src/management/TeamUsers.tsx
@@ -376,12 +376,21 @@ export const Team = () => {
     <div className='space-y-6'>
       <DataTable data={users || []} columns={users_columns} meta={{ title: 'Current Users' }} />
       {/* <InviteUsers /> */}
-      {invitationsData.length > 0 && (
-        <>
-          <h4 className='font-medium text-md'>Pending Invitations</h4>
-          <DataTable data={invitationsData || []} columns={invitations_columns} meta={{ title: 'Pending Invitations' }} />
-        </>
-      )}
+      {(invitationsData?.length === 0)
+        ? (
+          <>
+            <h4 className='text-2xl font-bold mr-auto'>Pending Invitations</h4>
+            <div className='flex items-center justify-center p-4 border rounded-md text-center'>
+              <span className='text-sm text-muted-foreground'>No pending invitations.</span>
+            </div>
+          </>
+        )
+        : (invitationsData.length > 0 && (
+          <>
+            <h4 className='font-medium text-md'>Pending Invitations</h4>
+            <DataTable data={invitationsData || []} columns={invitations_columns} meta={{ title: 'Pending Invitations' }} />
+          </>
+        ))}
     </div>
   );
 };
